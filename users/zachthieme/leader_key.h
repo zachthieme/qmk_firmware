a1f9ee98fb3ea5085f41820d077f24b27c2a43c2
Zach Thieme <zach@techsage.org>
2020-11-24 15:47:00 -0800

    forgot to comment out closing braces in the leader_keys.h

--- users/zachthieme/leader_key.h
+++ users/zachthieme/leader_key.h
@@ -1,5 +1,5 @@
 // TODO add any destructions and examples
-// 
+
 // LEADER_EXTERNS();
 //
 // void matrix_scan_user(void) {
@@ -15,32 +15,32 @@
 //     SEQ_ONE_KEY(KC_L) {
 //
 //     }
-
-    // SEQ_ONE_KEY(Z_KVM_1) {
-    //   // change to KVM1 - my PC
-    //   layer_on(0);
-    //   SEND_STRING(SS_TAP(KC_LCTL) SS_TAP(KC_LCTL) "1");
-    // }
-
-    // SEQ_ONE_KEY(Z_KVM_2) {
-    //   // change to KVM2 - my mac
-    //   // layer 1
-    //   layer_on(1);
-    //   SEND_STRING(SS_TAP(KC_LCTL) SS_TAP(KC_LCTL) "2");
-    //
-    // }
-    //
-    // SEQ_ONE_KEY(Z_KVM_3) {
-    //   // change to KVM3 - work laptop
-    //   // layer 0
-    //   layer_on(0);
-    //   SEND_STRING(SS_TAP(KC_LCTL) SS_TAP(KC_LCTL) "3");
-    // }
-
+//
+//     SEQ_ONE_KEY(Z_KVM_1) {
+//       // change to KVM1 - my PC
+//       layer_on(0);
+//       SEND_STRING(SS_TAP(KC_LCTL) SS_TAP(KC_LCTL) "1");
+//     }
+//
+//     SEQ_ONE_KEY(Z_KVM_2) {
+//       // change to KVM2 - my mac
+//       // layer 1
+//       layer_on(1);
+//       SEND_STRING(SS_TAP(KC_LCTL) SS_TAP(KC_LCTL) "2");
+//
+//     }
+//
+//     SEQ_ONE_KEY(Z_KVM_3) {
+//       // change to KVM3 - work laptop
+//       // layer 0
+//       layer_on(0);
+//       SEND_STRING(SS_TAP(KC_LCTL) SS_TAP(KC_LCTL) "3");
+//     }
+//
 // helper
 //  TODO look into secrets from drashna
-    // SEQ_ONE_KEY(KC_GESC) {
-    //     SEND_STRING("45!");
-    // }
-  }
-}
+//     SEQ_ONE_KEY(KC_GESC) {
+//         SEND_STRING("45!");
+//     }
+//   }
+// }
